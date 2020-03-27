4512eda4c9f1c87f54b05c978c8cdc2ac1f056cf
unknown <nkosimova06@gmail.com>
2020-03-27 20:43:40 +0500

    hw3problem2 final

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ namespace Problem2
             //Problem2;
             int a, b, c, d;
             int result;
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Введите через пробел 4 числа:");
             String[] nums = Console.ReadLine().Split();
             a = int.Parse(nums[0]);
             b = int.Parse(nums[1]);
@@ -16,8 +18,10 @@ namespace Problem2
             d = int.Parse(nums[3]);
             if (a == b & b == c & c == d){
                 result = a*b*c*d;
-            } else if (a >= b && b >= c && c >= d) {
+                Console.WriteLine($"res = {result}");
+            } else if (a < b && b < c && c < d) {
                 result = Math.Min(Math.Min(a,b),Math.Min(c,d));
+                Console.WriteLine($"res = {result}");
             } else {
                 Console.WriteLine("Числа расположены не по возрастанию");
             }
