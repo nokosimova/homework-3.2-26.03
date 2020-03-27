61c69e4322c565d445209c8ef849fb4fca58a7cc
unknown <nkosimova06@gmail.com>
2020-03-27 20:35:11 +0500

    hw3prob2 second

--- Program.cs
+++ Program.cs
@@ -9,12 +9,17 @@ namespace Problem2
             //Problem2;
             int a, b, c, d;
             int result;
+            String[] nums = Console.ReadLine().Split();
+            a = int.Parse(nums[0]);
+            b = int.Parse(nums[1]);
+            c = int.Parse(nums[2]);
+            d = int.Parse(nums[3]);
             if (a == b & b == c & c == d){
                 result = a*b*c*d;
             } else if (a >= b && b >= c && c >= d) {
                 result = Math.Min(Math.Min(a,b),Math.Min(c,d));
             } else {
-                Console.WriteLine("Числа расположены не по возрастанию")
+                Console.WriteLine("Числа расположены не по возрастанию");
             }
         }
     }
