9bd9efb01c64aa726153f57607a99c1b71e5a3b0
unknown <nkosimova06@gmail.com>
2020-03-27 20:30:53 +0500

    hw3prob2 fisrt

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace Problem2
     {
         static void Main(string[] args)
         {
-            //Problem3;
+            //Problem2;
+            int a, b, c, d;
+            int result;
+            if (a == b & b == c & c == d){
+                result = a*b*c*d;
+            } else if (a >= b && b >= c && c >= d) {
+                result = Math.Min(Math.Min(a,b),Math.Min(c,d));
+            } else {
+                Console.WriteLine("Числа расположены не по возрастанию")
+            }
         }
     }
 }
